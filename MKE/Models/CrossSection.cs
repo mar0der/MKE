fba48573883f9badba98da353fae69e95c79529c
Petar Petkov <petar.b.petkov@gmail.com>
2023-10-27 12:47:27 +0400

    Adding Cross section and Material classes with lot of properties

--- MKE/Models/CrossSection.cs
+++ MKE/Models/CrossSection.cs
@@ -2,5 +2,58 @@
 {
     public class CrossSection
     {
+        public int Id { get; set; }  // Unique identifier for the cross section
+
+        // General Parameters
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Material { get; set; }
+
+        // Geometric Parameters
+        public double Area { get; set; }
+        public double Perimeter { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double FlangeWidth { get; set; }
+        public double FlangeThickness { get; set; }
+        public double WebThickness { get; set; }
+        public double InnerDiameter { get; set; }
+        public double OuterDiameter { get; set; }
+        public double Radius { get; set; }
+
+        // Centroidal Parameters
+        public double CentroidX { get; set; }
+        public double CentroidY { get; set; }
+
+        // Moment of Inertia
+        public double Ix { get; set; }
+        public double Iy { get; set; }
+
+        // Section Modulus
+        public double Sx { get; set; }
+        public double Sy { get; set; }
+
+        // Torsional Constants
+        public double J { get; set; }
+        public double Cw { get; set; }
+
+        // Other Parameters
+        public double ShearAreaX { get; set; }
+        public double ShearAreaY { get; set; }
+        public double Zx { get; set; }
+        public double Zy { get; set; }
+        public double RadiusOfGyrationX { get; set; }
+        public double RadiusOfGyrationY { get; set; }
+        public double Angle { get; set; }
+
+        // Additional Notes or Remarks
+        public string Remarks { get; set; }
+
+        // Constructor
+        public CrossSection()
+        {
+            // Initialization logic can be placed here if needed.
+        }
     }
+
 }
--- MKE/Models/Material.cs
+++ MKE/Models/Material.cs
@@ -8,5 +8,40 @@ namespace MKE.Models
 {
     public class Material
     {
+        public int Id { get; set; }  // Unique identifier for the material
+
+        // General Parameters
+        public string Name { get; set; }
+        public string Type { get; set; }  // E.g., Steel, Concrete, Wood, etc.
+        public string Description { get; set; }  // A brief description or remarks
+
+        // Mechanical Properties
+        public double ElasticModulus { get; set; }  // Young's Modulus (E)
+        public double ShearModulus { get; set; }  // (G)
+        public double PoissonRatio { get; set; }  // (ν)
+
+        // Strength Properties
+        public double YieldStrength { get; set; }  // (Fy)
+        public double TensileStrength { get; set; }  // (Fu)
+        public double CompressiveStrength { get; set; }
+        public double ShearStrength { get; set; }
+
+        // Density and Weight
+        public double Density { get; set; }  // Mass per unit volume
+        public double WeightPerUnitVolume { get; set; }  // Weight per unit volume, e.g., kN/m^3
+
+        // Thermal Properties
+        public double CoefficientOfThermalExpansion { get; set; }  // α
+        public double ThermalConductivity { get; set; }  // k
+
+        // Other Relevant Properties (Depending on the type of material)
+        public double DampingRatio { get; set; }  // Used for dynamic analysis
+        public double FatigueStrength { get; set; }  // For materials prone to fatigue
+
+        // Constructor
+        public Material()
+        {
+            // Initialization logic can be placed here if needed.
+        }
     }
 }
