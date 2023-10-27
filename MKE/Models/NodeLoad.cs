cadbcbd79c8ba64e1ab8f52d0dc2918c89ce9224
Petar Petkov <petar.b.petkov@gmail.com>
2023-10-27 16:33:26 +0400

    added nodal load class model

--- MKE/Models/NodeLoad.cs
+++ MKE/Models/NodeLoad.cs
@@ -1,10 +1,24 @@
 ﻿namespace MKE.Models
 {
+
     public class NodeLoad
     {
+        public int Id { get; set; }  // Unique identifier for the nodal load
+
+        public double ForceX { get; set; }  // Force along the X direction
+        public double ForceY { get; set; }  // Force along the Y direction
+
+        public double MomentZ { get; set; }  // Moment (or torque) about the Z axis
+
+        public Node AssociatedNode { get; set; }  // The node where this load is applied
+
+        // Constructor
         public NodeLoad()
         {
-
+            // By default, all forces and moments are initialized to zero.
+            ForceX = 0.0;
+            ForceY = 0.0;
+            MomentZ = 0.0;
         }
     }
 }
---
+++ MKE/Models/NodeSupport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MKE.Models
+{
+    public class NodeSupport
+    {
+        public int Id { get; set; }  // Unique identifier for the support
+
+        public bool IsFixedInX { get; set; }  // Restrict translation along X
+        public bool IsFixedInY { get; set; }  // Restrict translation along Y
+
+        public bool IsRotationFixed { get; set; }  // Restrict rotation in the plane
+
+        public Node AssociatedNode { get; set; }  // The node where this support is applied
+
+        // Constructor
+        public NodeSupport()
+        {
+            // By default, all movements and rotations are allowed (set to false).
+            IsFixedInX = false;
+            IsFixedInY = false;
+            IsRotationFixed = false;
+        }
+
+        // You can also have methods to quickly set common support types like pinned, fixed, roller, etc.
+    }
+}
