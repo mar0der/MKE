11142b2ec27bed6eafd3b1683e3c46a82436bf79
Petar Petkov <petar.b.petkov@gmail.com>
2023-10-28 22:08:19 +0400

    elements improvement

--- MKE/Models/Element.cs
+++ MKE/Models/Element.cs
@@ -1,30 +1,43 @@
-﻿using MKE.Services;
-using System.Text.Json.Serialization;
+﻿using System;
 
 namespace MKE.Models
 {
     public class Element
     {
         public int Id { get; set; }
-        public int StartNodeId { get; set; } // Replaced Node with Node ID
-        public int EndNodeId { get; set; }   // Replaced Node with Node ID
-        public Material Material { get; set; }
-        public CrossSection CrossSection { get; set; }
-        [JsonIgnore]
-        public Node StartNode => DatabaseService.Instance.GetNodeById(StartNodeId);
-        [JsonIgnore]
-        public Node EndNode => DatabaseService.Instance.GetNodeById(EndNodeId);
 
+        // Nodes defining the element.
+        public Node StartNode { get; set; }
+        public Node EndNode { get; set; }
 
-        public Element() { }
+        // Orientation of the element in the global system.
+        public double Orientation => ComputeOrientation();
 
-        public Element(int startNodeId, int endNodeId, Material material, CrossSection crossSection)
+        // Transformation matrix to move between local and global systems.
+        public double[,] TransformationMatrix => ComputeTransformationMatrix();
+
+        // ... Other properties related to the element ...
+
+        private double ComputeOrientation()
         {
-            StartNodeId = startNodeId;
-            EndNodeId = endNodeId;
-            Id = IdGeneratorService.Instance.GetNextElementId();
-            Material = material;
-            CrossSection = crossSection;
+            // Assuming 2D for simplicity:
+            return Math.Atan2(EndNode.Y - StartNode.Y, EndNode.X - StartNode.X);
         }
+
+        private double[,] ComputeTransformationMatrix()
+        {
+            // For a 2D system, the transformation matrix can be defined using the orientation angle.
+            // Here's a basic idea for a 2x2 matrix. This can be extended for more DOFs as needed.
+
+            double cosTheta = Math.Cos(Orientation);
+            double sinTheta = Math.Sin(Orientation);
+
+            return new double[,]
+            {
+                { cosTheta, -sinTheta },
+                { sinTheta, cosTheta }
+            };
+        }
+
     }
 }
